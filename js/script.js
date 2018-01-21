4a076d9499e22bf0693b49ecf3ef7f9d7cd18c0b
JUNN <junn279@gmail.com>
2018-01-21 22:38:40 +0900

    fix

--- js/script.js
+++ js/script.js
@@ -41,40 +41,82 @@ function imageSetting(html)
 	}
 	return html_change;
 }
+function getFirstImage(obj)
+{
+	if(obj.hasOwnProperty("json_metadata") == false || obj.json_metadata.length == 0)
+	{
+		src = args.null_image_url;
+	}
+	else
+	{
+		/*{"tags":["kr","kr-med","mediteam","scuba","diving"],"image":["https://junn.in/wp-content/uploads/2017/09/Ear-Picture.jpg","https://junn.in/wp-content/uploads/2017/09/ET_Normal-300x142.jpg","https://junn.in/wp-content/uploads/2017/09/equalizing-300x154.png","https://junn.in/wp-content/uploads/2017/09/screen.png","https://junn.in/wp-content/uploads/2017/09/normal_state-300x200.png","https://junn.in/wp-content/uploads/2017/09/press2-216x300.png","https://junn.in/wp-content/uploads/2017/09/val.png","https://junn.in/wp-content/uploads/2017/09/inflam.png","https://junn.in/wp-content/uploads/2017/09/inflam2.png","https://junn.in/wp-content/uploads/2017/09/OME_AOM_01-300x298.jpg"],"links":["https://www.drsethi.com.sg/eustachian-tube-dysfuction/"],"app":"steemit/0.1","format":"html"}*/
+		var articleMeta = JSON.parse(obj.json_metadata);
+		//console.log(i,authorMeta,authorMeta.hasOwnProperty("image"));
+		//article.hasOwnProperty("json_metadata")
+		if(articleMeta.hasOwnProperty("image") == true && articleMeta.image.length > 0)
+		{
+			src = articleMeta.image[0];
+		}
+		else
+		{
+			src = args.null_image_url;
+		}
+	   	//$('#wrapper #main .meta .author img').attr('style','width:100%;max-width:846px; height:auto;');
+	}
+	return src;
+}
+function getTags(obj)
+{
+	if(obj.hasOwnProperty("json_metadata") == false || obj.json_metadata.length == 0)
+	{
+		return null;
+	}
+	else
+	{
+		/*{"tags":["kr","kr-med","mediteam","scuba","diving"],"image":["https://junn.in/wp-content/uploads/2017/09/Ear-Picture.jpg","https://junn.in/wp-content/uploads/2017/09/ET_Normal-300x142.jpg","https://junn.in/wp-content/uploads/2017/09/equalizing-300x154.png","https://junn.in/wp-content/uploads/2017/09/screen.png","https://junn.in/wp-content/uploads/2017/09/normal_state-300x200.png","https://junn.in/wp-content/uploads/2017/09/press2-216x300.png","https://junn.in/wp-content/uploads/2017/09/val.png","https://junn.in/wp-content/uploads/2017/09/inflam.png","https://junn.in/wp-content/uploads/2017/09/inflam2.png","https://junn.in/wp-content/uploads/2017/09/OME_AOM_01-300x298.jpg"],"links":["https://www.drsethi.com.sg/eustachian-tube-dysfuction/"],"app":"steemit/0.1","format":"html"}*/
+		var articleMeta = JSON.parse(obj.json_metadata);
+		//console.log(i,authorMeta,authorMeta.hasOwnProperty("image"));
+		//article.hasOwnProperty("json_metadata")
+		if(articleMeta.hasOwnProperty("tags") == true && articleMeta.tags.length > 0)
+		{
+			return articleMeta.tags;
+		}
+	}
+	return null;
+}
 
 /*
 주의 사항 last_update 가 수정여부를 평가하는 기준이 되어야 한다.
 */
 jQuery(document).ready(function($) 
 {
+	//alert(args.template_permlink);
+	var url =  args.giphy_url;
+	var msg = args.target_id + '이 작성한 글을 확인 중 입니다.<br><img style="width:50px;height:50px;" src="' + url + '"/>';
+	jQuery("#"+args.status_view).html(msg);
+	//jQuery("#"+args.status_view).notify(args.target_id, "success",{ position:"bottom" });
 	console.log("..sync..");
 	var today = new Date();
 	today.setMinutes(today.getMinutes()-today.getTimezoneOffset());
 	var today_str = today.format('yyyy-mm-dd') + 'T' + today.format('HH:MM:ss');
 
-	steem.api.getAccounts(['doctorfriend'], function(err, accounts)
+	/*steem.api.getAccounts(['doctorfriend'], function(err, accounts)
 	{
 		console.log(err,accounts);
-	});
-	
-	steem.api.getDiscussionsByAuthorBeforeDate("doctorfriend",
+	});*/
+	//console.log(args.target_id,today_str);
+	steem.api.getDiscussionsByAuthorBeforeDate(args.target_id,
 		'',today_str,100, 
 		function(err, result) {
-    	//console.log(err, result);
+    	console.log(err, result);
     	var content_arr = new Array();
 	    //console.log("contents", content_obj);
 	 	for(var i=0;i<result.length;i++)
     	{
     		content_arr[content_arr.length] = result[i];
     	}
-	 	//console.log("contents_arr", content_arr);
-	 	content_arr.sort(compare);
-	 	console.log(content_arr);
+	 	content_arr.sort(compare);//Sorting
 	 	var converter = new showdown.Converter();
-    	/*text = result.body,
-    	html_body = converter.makeHtml(text);
-    	html_body = changeYouTubeTag(changeBrTag(imageSetting(html_body)));*/
-
 	 	var json_arr = new Array();
 	 	for(var i=0;i<content_arr.length;i++)
 	 	{
@@ -98,40 +140,46 @@ jQuery(document).ready(function($)
 	 		json_element.permlink = permlink;
 	 		json_element.title = title;
 	 		json_element.steem_url = steem_url;
+	 		json_element.first_image = getFirstImage(article);
+	 		json_element.tags = getTags(article);
 	 		
 	 		json_arr[json_arr.length] = json_element;
 	 	}
 	 	var post_json = JSON.parse(JSON.stringify(json_arr));	//Object to JSON : OK
-	 	//console.log(post_json[0]);
-
-	 	/*$.ajax({
-	 		url:"https://yjnjg.net/sync",
+	 	console.log("POST",post_json);
+	 	
+	 	$.ajax({
+	 		url:args.template_permlink,
 	 		type:'post',
 	 		dataType:'json',
-	 		data:{"json_data":post_json,
-	 		"cmd": "add2"},
-	 		success:
-	    			function(data){      
-      					console.log("result");
-      					console.log(data['code']);
-             			console.log("A",data['title']); // John     
-             			console.log("B1",data['time']); // John  
-             			console.log("B2",data['time2']); // John  
-             			console.log("B3",data['time3']); // John  
-             			console.log("C",data['author']);
-             			console.log("D",data['return']); 
-             			console.log("e",data['error_msg']);      
-			             if(data['result'] == 'success')
-			             {
-			             	var count = data['insert_count'];
-			             	$("#jslp_sync_view").html("<p>"+count+"</p>");
-			             }
-			             else
-			             {
-			             	$("#jslp_sync_view").html("<p>FAIL</p>");	
-			             }
-      				}
-      	});*/
+	 		data:{"json_data":post_json},
+	 		error : function(error) {
+    		    //alert("Error!");
+    		    console.log(error);
+    		    console.log("error");
+    		},
+	 		success: function(data){      
+      					console.log(data);
+			            if(data['result'] == 'success')
+			            {
+			             	var insert_count = data['insert_count'];
+			             	var insert_error = data['insert_error_count'];
+			             	var update_count = data['update_count'];
+			             	var update_error = data['update_error_count'];
+			             	$("#"+args.status_view).html("<p>등록 : "+insert_count+
+			             		"</p><p>오류 : "+insert_error+"</p><p>업데이트 : "
+			             		+update_count+"</p><p>오류 : "+update_error+"</p>");
+			            }
+			            else
+			            {
+			             	$("#"+args.status_view).html("<p>FAIL</p>");	
+			            }
+      		},
+      		complete : function() {
+        		//alert("complete!");   //종결된 후 
+        		console.log("complete");
+   			 }
+      	});
 	});
 	
 
--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: JUNN Steemit & Wordpress Sync
-Plugin URI: https://github.com/junn279/junn-steemit-latest-posts
-Description: Bagic Latest Posts using Steem API.
+Plugin URI: https://github.com/junn279/junn-steemit-wordpress-sync
+Description: Get Steem Posts to Wordpress using Steem API.
 Version: 1.0
 Author: Junn
 Author URI: http://junn.in
@@ -23,68 +23,83 @@ class junn_steemit_wordpress_sync extends WP_Widget {
 
 	public function widget( $args, $instance ) 					//Contents of View(실제 화면)
 	{
+		$target_id = $instance['target_id'];
+		$template_permlink = $instance['template_permlink'];
 		$title = apply_filters( 'widget_title', $instance[ 'title' ] );
 		$blog_title = get_bloginfo( 'name' );
 		$tagline = get_bloginfo( 'description' );
 		echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title'];
 
+		$view_name = "jsws_status_view";
 		/*<p><strong>Search Type:</strong> <?php echo $search_option ?></p>
 		<p><strong>Keyword:</strong> <?php echo $keyword ?></p>
 		<p><strong>Count:</strong> <?php echo $nrow ?></p>*/
 		?>
-		<p><div id="jslp_sync_view" style="width:100%;"><img style="width:50px;height:50px;" src="<?=plugins_url('img/giphy.gif',__FILE__)?>"/></div></p>
+		<p><div id="<?=$view_name?>" style="width:100%;"><img style="width:50px;height:50px;" src="<?=plugins_url('img/giphy.gif',__FILE__)?>"/></div></p>
 		<?php 
-		/*
+		
 		$translation_array = array(
-			'query_tag' => $query_tag,
-			'no_profile_image' => plugins_url("img/no-profile.png",__FILE__),
-			'nrow' => $nrow
+			'target_id' => $target_id,
+			'status_view' => $view_name,
+			'template_permlink' => home_url()."/".$template_permlink,
+			'giphy_url'=> plugins_url("img/giphy.gif",__FILE__),
+			'null_image_url' => plugins_url("img/mediteam.png",__FILE__)
+
 		);
-		*/
-		wp_enqueue_script('jslp-steem-js',plugins_url('js/script.js',__FILE__));
-		//wp_localize_script('jslp-steem-js', 'args', $translation_array );
+		wp_enqueue_script('jsws-main-js',plugins_url('js/script.js',__FILE__));
+		wp_localize_script('jsws-main-js', 'args', $translation_array );
+
 		?>
 		 <?php echo $args['after_widget'];
 	}
 
 	public function form( $instance ) 							//Appearnce -> Widget -> Setting에서 보이는 부분
 	{
-	  $title = ! empty( $instance['title'] ) ? $instance['title'] : ''; ?>
+	  $title = ! empty( $instance['title'] ) ? $instance['title'] : ''; 
+	  $target_id = ! empty( $instance['target_id'] ) ? $instance['target_id'] : '';
+	  $template_permlink = ! empty( $instance['template_permlink'] ) ? $instance['template_permlink'] : ''; 
+	  ?>
 	  <p>
 	    <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
 	    <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
 	  </p>
-	  
+	  <p>
+	    <label for="<?php echo $this->get_field_id( 'target_id' ); ?>">ID :</label>
+	    <input type="text" id="<?php echo $this->get_field_id( 'target_id' ); ?>" name="<?php echo $this->get_field_name( 'target_id' ); ?>" value="<?php echo esc_attr( $target_id ); ?>" />
+	  </p>
+	   <p>
+	    <label for="<?php echo $this->get_field_id( 'template_permlink' ); ?>">Template Page Permlink :</label>
+	    <input type="text" id="<?php echo $this->get_field_id( 'template_permlink' ); ?>" name="<?php echo $this->get_field_name( 'template_permlink' ); ?>" value="<?php echo esc_attr( $template_permlink ); ?>" />
+	  </p>
 	  <?php 
 	}
-	public function update( $new_instance, $old_instance ) 		//Appearnce -> Widget -> Setting -> Click Save Button
+	public function update( $new_instance, $old_instance ) //Appearnce -> Widget -> Setting -> Click Save Button
 	{
   		$instance = $old_instance;
   		$instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
-  		/*$instance[ 'search_option'] = strip_tags( $new_instance ['search_option']);
-  		$instance[ 'keyword'] = strip_tags( $new_instance ['keyword']);
-  		$num = strip_tags( $new_instance ['nrow']);
-  		if($num > 20)$num = 20;
-  		if($num < 0)$num = 1;
-  		$instance[ 'nrow'] = $num;*/
+  		$instance[ 'target_id' ] = strip_tags( $new_instance[ 'target_id' ] );
+  		$instance[ 'template_permlink' ] = strip_tags( $new_instance[ 'template_permlink' ] );
+  		
   		return $instance;
 	}
 }
 
 	
-function jslp_load_steemjs() {
+function jsws_load_steemjs() {
 	//js 파일 추
-	wp_enqueue_script('include-steem-js', '//cdn.steemjs.com/lib/latest/steem.min.js');
-	wp_enqueue_script('showdown-js', '//cdn.rawgit.com/showdownjs/showdown/1.7.3/dist/showdown.min.js');	
-	wp_enqueue_script('dateformat-js', plugins_url('js/date.format.js',__FILE__));	
-	wp_enqueue_script('notify_js', plugins_url('js/notify.min.js',__FILE__))
+	
+	
+	wp_enqueue_script('jsws_include-steem-js', '//cdn.steemjs.com/lib/latest/steem.min.js');
+	wp_enqueue_script('jsws_showdown-js', '//cdn.rawgit.com/showdownjs/showdown/1.7.3/dist/showdown.min.js');	
+	wp_enqueue_script('jsws_dateformat-js', plugins_url('js/date.format.js',__FILE__));	
+	wp_enqueue_script('jsws_notify_js', plugins_url('js/notify.min.js',__FILE__),array( 'jquery' ));
 }
 
-add_action( 'wp_enqueue_scripts', 'jslp_load_steemjs' );
+add_action( 'wp_enqueue_scripts', 'jsws_load_steemjs' );
 
-function jslp_register_my_widget() { 
+function jsws_register_my_widget() { 
   register_widget( 'junn_steemit_wordpress_sync' );	//Save as [Class Name]
 }
-add_action( 'widgets_init', 'jslp_register_my_widget' );	//Don`t change
+add_action( 'widgets_init', 'jsws_register_my_widget' );	//Don`t change
 
 ?>
